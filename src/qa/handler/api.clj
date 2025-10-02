155959fe7ddc8af0615d37d14085bd523176ceca
hkimjp <hkim@hkim.jp>
2025-10-02 23:26:27 +0900

    under construction

--- src/qa/handler/api.clj
+++ src/qa/handler/api.clj
@@ -10,13 +10,20 @@
    ; [qa.boundary.goods :as goods]
    ; [qa.boundary.questions :as questions]
    [qa.boundary.readers :as readers]
-   [taoensso.timbre :refer [info debug]]))
+   [taoensso.timbre :refer [info]]))
 
 ; (defmethod ig/init-key :qa.handler.core/readers [_ {:keys [db]}]
 ;   (fn [{[_ path n] :ataraxy/result}]
 ;     (readers-page (readers/fetch-readers db path n @since) @since)))
 
 (defmethod ig/init-key :qa.handler.api/since [_ {:keys [db]}]
-  (fn [{[_ params] :ataraxy/result}]
-    [::response/ok (str params)]))
+  (fn [{[_ date] :ataraxy/result}]
+    (let [ret (->> (readers/fetch-readers db "qs" 0 date)
+                   (map :login)
+                   set
+                   vec)]
+      {:status 200
+       :header {"content-type" "application/json"}
+       :body {:since (str vec)}})))
+
 
