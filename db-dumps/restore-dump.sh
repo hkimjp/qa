ec4151ff7274de95fbe14b1a5351ab4ec82fa2a7
hkimjp <hkim@hkim.jp>
2025-10-03 10:53:51 +0900

    updated restore-dump.sh

--- db-dumps/restore-dump.sh
+++ db-dumps/restore-dump.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 if [ -z "$1" ]; then
+  echo "usage: $0 file.dump"
+  exit
+fi
+
+dropdb qa
+createdb qa
+pg_restore -U postgres -h localhost -Fc -d qa $1
+
+#!/bin/sh
+if [ -z "$1" ]; then
   echo "usage: $0 file.sql"
   exit
 fi
 
-#dropdb qa
-#createdb qa
-pg_restore -U postgres -h db --port 15432 -Fc -d qa $1
+dropdb qa
+createdb qa
+pg_restore -U postgres -h localhost -Fc -d qa $1
+
