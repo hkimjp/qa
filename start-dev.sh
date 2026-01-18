dd0b9507f353b92641bb4c685592e9efe6f1f6ec
hkimjp <hkim@hkim.jp>
2026-01-18 11:20:54 +0900

    updated start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-@echo 'duct app'
-@echo 'after jacking in the repl, development wil start by (dev), (go).'
+echo 'duct app'
+echo 'after jacking in the repl, development wil start by (dev), (go).'
 
 export QA_DEV=true
 export PORT=3000
