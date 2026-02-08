114a750875118ac24f093e8f5ddc02916bd99a25
hkimjp <hkim@hkim.jp>
2026-02-08 16:07:07 +0900

    updated password

--- start-dev.sh
+++ start-dev.sh
@@ -5,5 +5,5 @@ echo 'after jacking in the repl, development wil start by (dev), (go).'
 
 export QA_DEV=true
 export PORT=3000
-export DATABASE_URL='jdbc:postgresql://db/qa?user=postgres&password=password'
+export DATABASE_URL='jdbc:postgresql://db/qa?user=postgres&password=hamlet'
 lein repl
